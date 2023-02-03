fb7758a2df4c8ee6897c0a250b42d2e492f94c08
Yasir19 <yasir19@example.com>
2023-02-02 23:29:28 -0600

    Git test v2

--- GitTest2.java
+++ GitTest2.java
@@ -1,5 +1,6 @@
-public class Main {
+public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("Git Test2");
+        System.out.println("Git Test v1");
     }
 }
